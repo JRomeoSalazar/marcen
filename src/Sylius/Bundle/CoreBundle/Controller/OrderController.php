870e39318ee6f5ad810d94a1bc19f5c82b9455e8
Jorge Romeo <jromeosalazar@gmail.com>
2014-03-25 13:02:20 +0100

    Completar pago y cambiar estado envío

--- src/Sylius/Bundle/CoreBundle/Controller/OrderController.php
+++ src/Sylius/Bundle/CoreBundle/Controller/OrderController.php
@@ -12,7 +12,11 @@
 namespace Sylius\Bundle\CoreBundle\Controller;
 
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\CoreBundle\Checkout\SyliusCheckoutEvents;
+use Sylius\Bundle\CoreBundle\OrderProcessing\StateResolver;
 use Sylius\Bundle\CoreBundle\SyliusOrderEvents;
+use Sylius\Bundle\PaymentsBundle\Model\PaymentInterface;
+use Sylius\Bundle\PaymentsBundle\SyliusPaymentEvents;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -71,5 +75,54 @@ class OrderController extends ResourceController
     private function getFormFactory()
     {
         return $this->get('form.factory');
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function completePaymentAction($id)
+    {
+        /* Obtenemos la order */
+        $order = $this->get('sylius.repository.order')->findOneById($id);
+        if (!isset($order)) {
+            throw new NotFoundHttpException('Requested ORDER does not exist. id:'.$id);
+        }
+
+        /* Obtenemos el estado completado y la cantidad total del pedido. */
+        $amount = $order->getTotal();
+        $state = PaymentInterface::STATE_COMPLETED;
+
+        /* Le decimos la cantidad que se ha pagado y que el pago está completado. */
+        $payment = $order->getPayment();
+        $previousState = $order->getPayment()->getState();
+        $payment->setAmount($amount);
+        $payment->setState($state);
+
+        if ($previousState !== $payment->getState()) {
+            $this->get('event_dispatcher')->dispatch(
+                SyliusPaymentEvents::PRE_STATE_CHANGE,
+                new GenericEvent($order->getPayment(), array('previous_state' => $previousState))
+            );
+        }
+
+        // Lanzamos el evento para actualizar la cantidad total del pedido en el pago (sólo si state == 'completed').
+        $this->get('event_dispatcher')->dispatch(
+            SyliusCheckoutEvents::FINALIZE_PRE_COMPLETE,
+            new GenericEvent($order)
+        );
+
+        /* Persistimos el pago */
+        $manager = $this->get('sylius.manager.payment');
+        $manager->persist($payment);
+        $manager->flush();
+
+        /* Mensaje de éxito */
+        $translator = $this->get('translator');
+        $this->get('session')->getFlashBag()->add('success', $translator->trans('sylius.payment.success', array(), 'flashes'));
+
+        /* Redirigimos */
+        return $this->redirect($this->generateUrl('sylius_backend_order_index'));
     }
 }
