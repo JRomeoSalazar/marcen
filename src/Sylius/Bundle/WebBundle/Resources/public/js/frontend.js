94e7962d5a27b9f7cc9eee953bba7dab760cc8aa
Ciclos Marcen <info@ciclosmarcen.com>
2014-10-24 21:36:32 +0200

    Ahora si que he arreglado el link

--- src/Sylius/Bundle/WebBundle/Resources/public/js/frontend.js
+++ src/Sylius/Bundle/WebBundle/Resources/public/js/frontend.js
@@ -21,15 +21,7 @@
 		$.cookieCuttr({
 			cookieAnalytics: false,
 			cookieMessage: 'Nosotros utilizamos cookies para brindarle la mejor experiencia posible en nuestro sitio. Al seguir utilizando nuestro sitio usted acepta nuestra <a href="{{cookiePolicyLink}}" title="Política de cookies">Política de cookies</a> y el uso de las mismas.',
-<<<<<<< HEAD
-<<<<<<< HEAD
-			cookiePolicyLink: '/disclaimer',
-=======
-			cookiePolicyLink: $cookies,
->>>>>>> marcen
-=======
 			cookiePolicyLink: cookies,
->>>>>>> marcen
 			cookieAcceptButtonText: 'Aceptar cookies'
 		});
 
