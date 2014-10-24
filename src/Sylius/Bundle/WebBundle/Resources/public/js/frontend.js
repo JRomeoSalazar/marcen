40a09f05af96ffd4d6c8c1e0543be13a420bbecd
Jorge Romeo <jromeosalazar@gmail.com>
2014-10-24 21:22:34 +0200

    Arreglar variable cookies

--- src/Sylius/Bundle/WebBundle/Resources/public/js/frontend.js
+++ src/Sylius/Bundle/WebBundle/Resources/public/js/frontend.js
@@ -21,7 +21,7 @@
 		$.cookieCuttr({
 			cookieAnalytics: false,
 			cookieMessage: 'Nosotros utilizamos cookies para brindarle la mejor experiencia posible en nuestro sitio. Al seguir utilizando nuestro sitio usted acepta nuestra <a href="{{cookiePolicyLink}}" title="Política de cookies">Política de cookies</a> y el uso de las mismas.',
-			cookiePolicyLink: $cookies,
+			cookiePolicyLink: cookies,
 			cookieAcceptButtonText: 'Aceptar cookies'
 		});
 
